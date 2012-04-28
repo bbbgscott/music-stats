8b05739b32b59ba9aa1b5a0b9abf33f58fc05514
bbbgscott <bbbgscott@gmail.com>
2012-04-28 14:57:41 -0700

    updated setup, added sample xml files

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 from distutils.core import setup
 
-setup(name='Music Stats'
-	  version='0.5'
-	  py_models=['main_interface'],
+setup(name='Music Stats',
+	  version='0.5',
+	  py_modules=['main_interface', 'MusicStats'],
+	  description='Music statistics utility',
+	  author='Brian Scott',
+	  author_email='bscott@csumb.edu',
+	  url='http://localhost',
 	  )
