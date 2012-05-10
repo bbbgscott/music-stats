94bf1179a1c6b54c65863f3f842f6b32724058ed
bbbgscott <bbbgscott@gmail.com>
2012-05-10 13:21:16 -0700

    updated xml parser done by Madalyn

--- parseXML.py
+++ parseXML.py
@@ -4,7 +4,7 @@ import os.path
 
 import sys
 
-def run():
+def run1():
 	x = XMLWriter("test.xml")
 
 	musicLibrary = x.start("musciLibrary")
@@ -20,49 +20,69 @@ def run():
 	x.end("song")
 	x.close(musicLibrary)
 
-run()
+run1()
 
-#root = ET.Element("musicLibrary")
-"""
-# build a tree structure
-root = ET.Element("musciLibrary")
+def run2():
+	# Create root
+	root = xml.Element('root')
 
-head = ET.SubElement(root, "head")
+	#Create a child element
+	child = xml.Element('child')
+	root.append(child)
 
-title = ET.SubElement(head, "title")
-title.text = "Page Title"
+	#Set element attributes
+	child.attrib['artist'] = " artist name goes here"
+	child.attrib['album'] = "album name goes here"
+	child.attrib['title'] = "title goes here"
+	child.attrib['year'] = "year goes here"
+	child.attrib['track'] = "track goes here"
+	child.attrib['genre'] = "genre goes here"
 
-body = ET.SubElement(root, "body")
-body.set("bgcolor", "#ffffff")
+	#Write to an .xml file on the hard drive
 
-body.text = "Hello, World!"
+	#Open a file
+	file = open("c:/library.xml", 'w')
 
-# wrap it in an ElementTree instance, and save as XML
-tree = ET.ElementTree(root)
-tree.write("page.xhtml")
+	#Create an ElementTree object from the root element
+	xml.ElementTree(root).write(file)
 
-"""
-"""
+	#Close the file
+	file.close()
 
-w = XMLWriter(sys.stdout)
-html = w.start("html")
+	// insert
 
-w.start("head")
-w.element("title", "my document")
-w.element("meta", name="generator", value="my application 1.0")
-w.end()
+	#Parse XML directly from the file path
+	tree = xml.parse("c:/library.xml")
 
-w.start("body")
-w.element("h1", "this is a heading")
-w.element("p", "this is a paragraph")
+	#Get the root node
+	rootElement = tree.getroot()
 
-w.start("p")
-w.data("this is ")
-w.element("b", "bold")
-w.data(" and ")
-w.element("i", "italic")
-w.data(".")
-w.end("p")
+	#Get a list of children elements with tag == "library"
+	libraryList = rootElem.findall("library")
 
-w.close(html)
-"""
+	#Check if any "songs" were found
+
+	      
+
+	# Set up XML text writer
+
+	XmlTextWriter xtw = new XmlTextWriter("Test.xml", System.Text.Encoding.UTF8);
+	xtw.Formatting = Formatting.Indented;
+	xtw.Indentation = 3;
+	xtw.IndentChar = ' ';
+	xtw.WriteStartDocument(true);
+	xtw.WriteStartElement("root");
+	for (int i = 0; i < 500000; i++)
+	  xtw.WriteElementString("child", "This is child number " + i.ToString());
+	xtw.WriteEndElement();
+	xtw.WriteEndDocument();
+	xtw.Close();
+
+	#append rows
+
+	XmlDocument doc = new XmlDocument();
+	doc.Load("library.xml");
+	XmlElement el = doc.CreateElement("child");
+	el.InnerText = "This row is being appended to the end of the document.";
+	doc.DocumentElement.AppendChild(el);
+	doc.Save("library.xml");
